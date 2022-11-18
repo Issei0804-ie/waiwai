518e74ea63fd322bcc244f8772bf9eaf85da5967
宮良一生 <issei0804@outlook.com>
2022-11-19 00:00:00 +0900

    format

--- main.py
+++ main.py
@@ -6,24 +6,27 @@ import torch
 import pytorch_lightning as pl
 from model import MaskModel
 
-aug_downscale_path = os.path.join("aug_dataset","downscale")
-aug_flip_path = os.path.join("aug_dataset","flip")
-aug_blur_path = os.path.join("aug_dataset","blur")
-aug_brightness_path = os.path.join("aug_dataset","brightness_contrast")
+aug_downscale_path = os.path.join("aug_dataset", "downscale")
+aug_flip_path = os.path.join("aug_dataset", "flip")
+aug_blur_path = os.path.join("aug_dataset", "blur")
+aug_brightness_path = os.path.join("aug_dataset", "brightness_contrast")
 with_mask_path = os.path.join("dataset", "with_mask")
 no_mask_path = os.path.join("dataset", "no_mask")
 
-with_mask_dirs = [with_mask_path, os.path.join(with_mask_path, aug_downscale_path), os.path.join(with_mask_path, aug_flip_path), os.path.join(with_mask_path, aug_blur_path), os.path.join(with_mask_path, aug_brightness_path)]
-no_mask_dirs = [no_mask_path, os.path.join(no_mask_path, aug_downscale_path), os.path.join(no_mask_path, aug_flip_path), os.path.join(no_mask_path, aug_blur_path), os.path.join(no_mask_path, aug_brightness_path)]
+with_mask_dirs = [with_mask_path, os.path.join(with_mask_path, aug_downscale_path),
+                  os.path.join(with_mask_path, aug_flip_path), os.path.join(with_mask_path, aug_blur_path),
+                  os.path.join(with_mask_path, aug_brightness_path)]
+no_mask_dirs = [no_mask_path, os.path.join(no_mask_path, aug_downscale_path), os.path.join(no_mask_path, aug_flip_path),
+                os.path.join(no_mask_path, aug_blur_path), os.path.join(no_mask_path, aug_brightness_path)]
 
 dataset = dataset_build.MaskDataset(with_mask_dirs, no_mask_dirs)
 
 n_samples = len(dataset)
 train_size = int(len(dataset) * 0.6)
-val_size = int(len(dataset)*0.2)
-test_size = n_samples - (train_size+val_size)
+val_size = int(len(dataset) * 0.2)
+test_size = n_samples - (train_size + val_size)
 
-train_dataset, val_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, val_size,test_size])
+train_dataset, val_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, val_size, test_size])
 
 train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=32, shuffle=True)
 val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=32, shuffle=True)
@@ -38,8 +41,6 @@ checkpoint = pl.callbacks.ModelCheckpoint(
     filename=f"bert_{str(datetime.datetime.today())}"
 )
 
-
-
 trainer = pl.Trainer(
     gpus=1,
     max_epochs=200,
@@ -51,4 +52,3 @@ trainer.fit(model, train_loader, val_loader)
 
 test = trainer.test(model, test_loader)
 print(test)
-
